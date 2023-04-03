d79bedf174e501d0c271a920d5ddbeb3cf39b50f
lazyhysong <37135523+eee0930@users.noreply.github.com>
2023-04-03 21:38:40 +0900

    refactor: 주석, 변수명 변경

--- setinterval/app.js
+++ setinterval/app.js
@@ -1,4 +1,5 @@
-const clockTitle = document.querySelector(".js-clock");
+const $clockTitle = document.querySelector("#jsClock");
+
 const XMAS_MONTH = 11; // Date는 실제 month보다 1 작은 결과가 나오므로
 const XMAS_DATE = 24;
 
@@ -11,11 +12,13 @@ function getDdayChristmas() {
   const CHRISTMAS = new Date(year, XMAS_MONTH, XMAS_DATE, 0, 0, 0, 0);
   const dDayTime = (CHRISTMAS.getTime() - date.getTime()) / 1000;
 
-  const dDayDate = Math.floor(dDayTime / 86400);
+  const dDayDate = Math.floor(dDayTime / 86400); // 60 * 60 * 24 = 86400
   const dDayHours = Math.floor((dDayTime % 86400) / 3600);
   const dDayMinutes = Math.floor(((dDayTime % 86400) % 3600) / 60);
   const dDaySecond = Math.floor(((dDayTime % 86400) % 3600) % 60);
-  clockTitle.innerHTML = `${dDayDate}d 
+
+  $clockTitle.innerHTML = 
+    `${dDayDate}d
     ${String(dDayHours).padStart(2, "0")}h 
     ${String(dDayMinutes).padStart(2, "0")}m 
     ${String(dDaySecond).padStart(2, "0")}s`;
